8dd566d9e29a596dc7334b19a28c129500247e63
syedammarilyas111 <syedammarilyas@hotmail.com>
2021-02-12 02:26:30 +0500

    till 9:35 timestamp

--- src/App.js
+++ src/App.js
@@ -1,3 +1,15 @@
 import {ChatEngine } from "react-chat-engine";
 
 import './App.css'
+
+const App = () => (
+
+    return(
+        <ChatEngine
+        height= "100vh"
+        projectID= ""
+        userName= ""
+        userSecret=""
+        />
+        )
+)
