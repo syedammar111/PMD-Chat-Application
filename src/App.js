37c8f31dead63f3d4461a3aca078965b8d574e49
syedammarilyas111 <syedammarilyas@hotmail.com>
2021-02-13 02:43:14 +0500

    13febchanges

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import {ChatEngine } from "react-chat-engine";
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from './components/ChatFeed';
 
 import './App.css'
 
-const App = () => (
+const App = () => {
 
-    return(
+    return (
         <ChatEngine
-        height= "100vh"
-        projectID= ""
-        userName= ""
-        userSecret=""
-        />
-        )
-)
+            height="100vh"
+            projectID="fd3609b6-5680-4e0c-9597-30fd17364dbf"
+            userName="admin"
+            userSecret="123"
+            renderChatFeed={(chatAppProps) => <ChatFeed {... chatAppProps} />}
+            />
+        );
+}
+
+export default App;
